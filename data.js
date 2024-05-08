61a0f2aefa3377fc3219694b66afd2318bba36e2
geoseo360 <geoseo360@gmail.com>
2024-05-08 17:16:20 +0400

    Commit

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-trisio_815148_img_20240508_154107_1715168591966",
-      "name": "trisio_815148_IMG_20240508_154107_1715168591966",
+      "id": "0--garden",
+      "name": " Garden",
       "levels": [
         {
           "tileSize": 256,
@@ -24,16 +24,23 @@ var APP_DATA = {
       ],
       "faceSize": 2000,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": -2.065443223030405,
+        "pitch": 0.1628592197249361,
+        "fov": 1.4966748790790423
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 0.09505581017574016,
+          "pitch": 0.08972739055950996,
+          "rotation": 0,
+          "target": "1-university"
+        }
+      ],
       "infoHotspots": []
     },
     {
-      "id": "1-trisio_815148_img_20240508_154346_1715168769423",
-      "name": "trisio_815148_IMG_20240508_154346_1715168769423",
+      "id": "1-university",
+      "name": "University",
       "levels": [
         {
           "tileSize": 256,
@@ -55,16 +62,23 @@ var APP_DATA = {
       ],
       "faceSize": 2000,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": 1.5648755215677035,
+        "pitch": 0.01599231585941041,
+        "fov": 1.2393174931806101
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -2.8339609064363565,
+          "pitch": -0.048191719421962986,
+          "rotation": 0,
+          "target": "2-hilton"
+        }
+      ],
       "infoHotspots": []
     },
     {
-      "id": "2-trisio_815148_img_20240508_155406_1715169971469",
-      "name": "trisio_815148_IMG_20240508_155406_1715169971469",
+      "id": "2-hilton",
+      "name": "Hilton",
       "levels": [
         {
           "tileSize": 256,
@@ -86,11 +100,18 @@ var APP_DATA = {
       ],
       "faceSize": 2000,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": 0.48449396293342417,
+        "pitch": 0.0736416957364554,
+        "fov": 1.199080235276018
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -1.318277361279355,
+          "pitch": 0.00918481633205026,
+          "rotation": 0,
+          "target": "1-university"
+        }
+      ],
       "infoHotspots": []
     }
   ],
@@ -98,7 +119,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
-    "fullscreenButton": false,
-    "viewControlButtons": false
+    "fullscreenButton": true,
+    "viewControlButtons": true
   }
 };
